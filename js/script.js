d76e5a6fe0778500b8e3287891b28d59e3106aaa
Bogus Franczak <bfranczak@gmail.com>
2019-01-13 21:38:42 +0100

    Implement mustache templates

--- js/script.js
+++ js/script.js
@@ -5,13 +5,24 @@ var carouselOptions = {
     cellSelector: '.carousel-slide',
     hash: true
 }
+var slideTemplate = document.querySelector('#slide-template').innerHTML;
+Mustache.parse(slideTemplate);
+
+var slidesHtml = '';
+slideData.forEach(function(slide, index) {
+    slide.id = index;
+    slidesHtml += Mustache.render(slideTemplate, slide);
+});
+
+document.querySelector('.carousel').insertAdjacentHTML('afterbegin',slidesHtml);
+
 var carousel = new Flickity('.carousel', carouselOptions);
 var progressBar = document.querySelector('.progress-bar');
 carousel.on('scroll', function(progress){
     progress = Math.max( 0, Math.min( 1, progress ) );
-    // console.log(progress);
     progressBar.style.width = progress * 100 + '%';
 })
+
 var btnReset = document.querySelector('.btn-restart');
 btnReset.addEventListener('click', function(){
     carousel.select(0, false, false);
