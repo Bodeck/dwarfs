0b4c71feccb1b50b92a6b2ee0d97b4035d7d2dfa
Bogus Franczak <bfranczak@gmail.com>
2019-01-13 16:26:57 +0100

    Add status bar and restart functionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ var carouselOptions = {
     cellAlign: 'right',
     contain: true,
     pageDots: false,
-    cellSelector: '.carousel-slide'
+    cellSelector: '.carousel-slide',
+    hash: true
 }
 var carousel = new Flickity('.carousel', carouselOptions);
+var progressBar = document.querySelector('.progress-bar');
+carousel.on('scroll', function(progress){
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    // console.log(progress);
+    progressBar.style.width = progress * 100 + '%';
+})
+var btnReset = document.querySelector('.btn-restart');
+btnReset.addEventListener('click', function(){
+    carousel.select(0, false, false);
+})
